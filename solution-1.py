d0e5d0c6e648c6a045974b57299ac13bd669cebb
Etem Kaya <etem.kaya@gmail.com>
2019-02-14 22:46:39 +0000

    Updated solution 1 file with Python code

--- solution-1.py
+++ solution-1.py
@@ -1,1 +1,7 @@
 # Solutions to problem 1
+
+x = int(input("Please enter a positive integer: "))
+if x < 0:
+    print('That is a negative integer. Please enter a positive integer')
+else:
+    print(sum(range(1,x+1)))
