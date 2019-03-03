4733716237005e5cf786d29097215cd3b377a25e
Etem Kaya <etem.kaya@gmail.com>
2019-03-03 23:40:56 +0000

    Solution to 'Problem-4' drafted.

---
+++ collatz.py
@@ -0,0 +1,30 @@
+# Etem Kaya 03-Mar-2019
+
+# Solution to Problem-4.
+# File name: "divisors.py".
+
+# Problem-4: Write a program that asks the user to input any positive integer
+# and outputs the successive values of the following calculation. At each step
+# calculate the next value by taking the current value and, if it is even,
+# divide it by two, but if it is odd, multiply it by three and add one. 
+# Have the program end if the current value is one.
+
+
+n = int(input("Please enter a positive integer: "))
+
+def collatz(n):
+    print(n, end=' ')
+    if n ==1:
+        print("Input is already 1")
+        return
+    
+    while n > 1:
+        if  n % 2 == 0: # when user input is an even number.
+            n = n//2 # set next user input number automatically.
+            print(n, end=' ')
+            
+        elif  n % 2 != 0: # when user input is an odd number.
+            n = (n * 3) + 1 # set next user input number automatically.
+            print(n, end=' ')
+
+collatz(n)
