8d59f4dc27fc93e7f9b833a5e8c9f4693dd12320
Etem Kaya <etem.kaya@gmail.com>
2019-03-03 16:17:42 +0000

    comments in 'Solution-1a' was updated

--- sumupto-1a.py
+++ sumupto-1a.py
@@ -1,33 +1,36 @@
 # Etem Kaya 22-Feb-2019
-# Solution-1a: Problem-1 first solution - "sumupto-1a.py".
-"""
-Problem-1: Write a program that asks the user to input 
-any positive integer less than 100 and outputs the 
-sum of all numbers between one and that number.
-"""
 
-while True: 
+# First solution to Problem-1 (Solution-1a)
+# Fine name: "sumupto-1a.py".
+
+# Problem-1: Write a program that asks the user to input 
+# any positive integer less than 100 and outputs the 
+# sum of all numbers between one and that number.
+
 # execute the code until a positive integer with less than 3 digit entered.
+while True: 
+# execute the code between try and exdep block and catch errors at the except function.
     try:
-        i = eval(input("Please enter a positive integer less than 100: ")) # evaluate the values entered.
+        # declare a user input varibale and evaluate it for tye of data to be entered.
+        i = eval(input("Please enter a positive integer less than 100: "))
 
-        if type(i) == str: # check if a string value entered.
+        if type(i) == str: # check a string value entered.
             print("You entered a string. Please try again.")
-            continue
-        elif type(i) == float: # check if a floating number entered.
+            continue # go to while True and continue executing the code 
+        elif type(i) == float: # check a floating number entered.
             print("You entered a floating number. Please try again.")
-            continue
-        elif type(i) == int and i < 0: # check if a negative integer entered.
+            continue # go to while True and continue executing the code
+        elif type(i) == int and i < 0: # check a negative integer entered.
             print("You entered a negative integer. Please try again.") 
-            continue
+            continue # go to while True and continue executing the code
         # max of 2 digit numbers allowed to prevent program crashing when large numbers entered.   
-        elif type(i) == int and i >= 100: # check if a positive integer with more than 2 digits entered.
+        elif type(i) == int and i >= 100: # check a positive integer with more than 2 digits entered.
             print("You entered a positive integer but more than 2 digits.")
             print("You are only allowed max of 2 digits. Please try again.")
         else:
-            # a positive integer with less than 3 digits entered.
-            print("The sum is", sum(range(i+1))) # add numbers from 0 upto the number entered.
-            break
+            # when a positive integer with less than 3 digits entered.
+            print("The sum is", sum(range(i+1))) # add up all numbers from 0 upto the number entered.
+            break # terminate the loop and end the program 
 
     except: 
     # an unacceptable value detected.
