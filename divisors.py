0b36058b3ae6475cfba07c3ff6b83660570a65ae
Etem Kaya <etem.kaya@gmail.com>
2019-03-03 16:41:25 +0000

    Solution to 'Problem-3' was drafted

---
+++ divisors.py
@@ -0,0 +1,8 @@
+# Etem Kaya 03-Mar-2019
+
+# Solution to Problem-3.
+# File name: "divisors.py".
+
+# Problem-3: Write a program that prints all numbers between
+# 1,000 and 10,000 that are divisible by 6 but not 12.
+
