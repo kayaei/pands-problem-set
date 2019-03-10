66547bfad28693087dd76ebe85087ba357d61d78
Etem Kaya <etem.kaya@gmail.com>
2019-03-10 20:01:37 +0000

    Solution to Problem-5 revised for divisiors.

--- primes.py
+++ primes.py
@@ -6,17 +6,27 @@
 # Problem-5: Write a program that asks the user to input a positive integer
 # and tells the user whether or not the number is a prime.
 
-def prime(n): # define prime number function.
+# import math function
+import math
+
+# define a function to check user input is prime or not.
+def prime(n): 
+    # integers < 2 not prime
     if n < 2: 
         print(n, "is not a prime.")  
+    # integers 2 is already prime.
     if n == 2:   
         print(n, "is a prime.")
-    # if even number and > 2
+    # all even numbers which are > 2 are not prime.
     if n > 2 and n % 2 == 0:   
         print(n, "is not a prime.")
-        return # terminate while loop.
 
-    for x in range(3, n):
+    # use math and square root with round down functions to find_
+    # possible number of divisors for the given input n.
+    divisors = math.floor(math.sqrt(n))
+
+    # loop to check only odd numbers from 3 up to the square root of the input n.
+    for x in range(3, divisors):
         # when it's even and > '2'.
         if n % x == 0:   
             print(n, "is not a prime.")
