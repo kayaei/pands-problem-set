942834f76e08b434b5b11c49f5cb11fe4c9b39cf
Etem Kaya <etem.kaya@gmail.com>
2019-03-10 18:52:43 +0000

    Solution to Problem-5 updated.

--- primes.py
+++ primes.py
@@ -6,22 +6,14 @@
 # Problem-5: Write a program that asks the user to input a positive integer
 # and tells the user whether or not the number is a prime.
 
-while True: # alway true loop until it breaks with user input.
+while True: # alway true until it breaks with user input.
     # try-except block to catch if no integer entered.
     try: 
         n = int(input("Please enter a positive integer: "))
-        # when it's a negative integer.
-        if n < 0: 
-            print("You entered a negative integer. Try again.") 
-            continue # keep asking until user enters an integer > 1.
-        # when it's zero .
-        if n == 0: 
-            print("Number '0' is not a prime. Try again.") 
-            continue # keep asking until user enters an integer > 1.
-        # when it's '1'.
-        if n == 1:   
-            print("Number '1' is not a prime. Try again.")
-            continue # keep asking until user enters an integer > 1.
+        # when it's < 2.
+        if n < 2: 
+            print(n, "is not a prime. Try again.") 
+            continue # keep asking until user enters an integer >= 2           
     # an error detected when a non-integer value entered.
     except ValueError: 
         print("You entered a non-integer value. Try again.")
@@ -29,12 +21,15 @@ while True: # alway true loop until it breaks with user input.
     else:
         # when it's '2'.
         if n == 2:   
-            print("Number '2' is a prime.")
+            print(n, "is a prime.")
             break # terminate while loop.
-        # when it's even and > '2'.
-        if n > 2 and n % 2 == 0:   
-            print("Number 'n' is not a prime.")
-            break # terminate while loop.
-    # when it's odd and > '2'.
-    print("Number 'n' is a prime.")
+        while n > 2:
+            for x in range(3, n+1):
+                # when it's even and > '2'.
+                if n % x == 0:   
+                    print(n, "is not a prime.")
+                    break # terminate while loop.
+                else:
+                    print(n, "is a prime.")
+                    break # terminate while loop.
     break # terminate while loop.
