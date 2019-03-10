711c57c1dfef9e7daae5c281f1cbef4d6cd275e7
Etem Kaya <etem.kaya@gmail.com>
2019-03-10 19:28:59 +0000

    Solution to Problem-5 revised for loop.

--- primes.py
+++ primes.py
@@ -6,30 +6,25 @@
 # Problem-5: Write a program that asks the user to input a positive integer
 # and tells the user whether or not the number is a prime.
 
-while True: # alway true until it breaks with user input.
-    # try-except block to catch if no integer entered.
-    try: 
-        n = int(input("Please enter a positive integer: "))
-        # when it's < 2.
-        if n < 2: 
-            print(n, "is not a prime. Try again.") 
-            continue # keep asking until user enters an integer >= 2           
-    # an error detected when a non-integer value entered.
-    except ValueError: 
-        print("You entered a non-integer value. Try again.")
-    # when it's >= 2.
-    else:
-        # when it's '2'.
-        if n == 2:   
-            print(n, "is a prime.")
+def prime(n): # define prime number function.
+    if n < 2: 
+        print(n, "is not a prime.")  
+    if n == 2:   
+        print(n, "is a prime.")
+    # if even number and > 2
+    if n > 2 and n % 2 == 0:   
+        print(n, "is not a prime.")
+        return # terminate while loop.
+
+    for x in range(3, n):
+        # when it's even and > '2'.
+        if n % x == 0:   
+            print(n, "is not a prime.")
             break # terminate while loop.
-        while n > 2:
-            for x in range(3, n+1):
-                # when it's even and > '2'.
-                if n % x == 0:   
-                    print(n, "is not a prime.")
-                    break # terminate while loop.
-            else:
-                print(n, "is a prime.")
-                break # terminate while loop.
-    break # terminate while loop.
+    else:
+        print(n, "is a prime.")
+
+n = int(input("Please enter a positive integer: "))
+
+# call the funtion
+prime(n)
