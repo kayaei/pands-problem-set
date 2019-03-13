ed2a161849431f307fb22c4d6471053cf5645f7b
Etem Kaya <etem.kaya@gmail.com>
2019-03-13 21:23:53 +0000

    Solution to Problem-6 drafted.

---
+++ secondstring.py
@@ -0,0 +1,11 @@
+# Etem Kaya 11-Mar-2019
+
+# Solution to Problem-6.
+# File name: "secondstring.py".
+
+# Problem-6: Write a program that takes a user input string and outputs every second word.
+
+My_sentence = "thios is a funny cheese shop but there is no mice in this shop"
+# print(text.partition(' '))[2] # cheese shop
+My_sentence = My_sentence.split(' ', 2)[1]
+print(My_sentence)
