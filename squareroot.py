bde75544e10009f8bf58a480d6301cb26b308fbd
Etem Kaya <etem.kaya@gmail.com>
2019-03-14 23:54:28 +0000

    Final update to 'Solution to Problem-7'

--- squareroot.py
+++ squareroot.py
@@ -10,11 +10,16 @@ while True:
     try:
         # The number to calculate the square root of.
         rootof = float(input("Enter a positive floating number to calculate its square root of: "))
+        
+        # check the number entered is a negative.
+        if rootof < 0: 
+            print("You entered a negative number. Try again.") 
+            continue # keep asking until user to enter a positive integer.
 
         # The initial estimate for the square root.
         estimate = float(input("Enter a number as an initial estimate for the square root: "))
 
-        # Keep calculating until the estimate is within 0.1 of rootof.
+        # Keep calculating until the estimate is within 0.01 of rootof.
         while abs((estimate * estimate) - rootof) > 0.1:
             # This is Neweton's method to improve the estimate.
             # Adapted from: https://tour.golang.org/flowcontrol/8
@@ -24,5 +29,5 @@ while True:
         print(f"The square root of {rootof} is approx. {estimate}.")
         break
 
-    except:
+    except ValueError:
         print("This is not a floating point number. Try again")
