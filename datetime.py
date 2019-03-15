c7562a81ba36fb914d7a34626f0334b441941069
Etem Kaya <etem.kaya@gmail.com>
2019-03-15 22:10:09 +0000

    'Solution to Problem-8' drafted.

---
+++ datetime.py
@@ -0,0 +1,15 @@
+# Etem Kaya 15-Mar-2019
+
+# Solution to Problem-8.
+# File name: "datetime.py".
+
+# Problem-8: Write a program that outputs today’s date and time in the format
+# ”Monday, January 10th 2019 at 1:15pm”.
+
+# import datetime module to get the current date.
+
+# import datetime module
+import datetime as dt
+
+t = dt.datetime.now()
+print(t)
