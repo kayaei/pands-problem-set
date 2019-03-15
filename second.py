0e11253fc5c35265a3ef2875dae7599f8c4d063c
Etem Kaya <etem.kaya@gmail.com>
2019-03-15 23:00:03 +0000

    'Solution-9' files 'second.py' and 'moby-dick.txt' drafted.

---
+++ second.py
@@ -0,0 +1,8 @@
+# Etem Kaya 15-Mar-2019
+
+# Solution to Problem-9.
+# File name: "dsecond.py".
+
+# Problem-9: Write a program that reads in a text file and outputs every second line. 
+# The program should take the filename from an argument on the command line.
+
