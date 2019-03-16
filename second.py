fa21171ca7c02c81288c952fd4119ac0960339ff
Etem Kaya <etem.kaya@gmail.com>
2019-03-16 17:59:33 +0000

    Final update on 'Solution to Problem-9'.

--- second.py
+++ second.py
@@ -1,7 +1,7 @@
 # Etem Kaya 15-Mar-2019
 
 # Solution to Problem-9.
-# File name: "dsecond.py".
+# File name: "second.py".
 
 # Problem-9: Write a program that reads in a text file and outputs every second line. 
 # The program should take the filename from an argument on the command line.
