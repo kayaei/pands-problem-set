bb2cefbe73defc2455ab56d5aff3495044477edb
Etem Kaya <etem.kaya@gmail.com>
2019-03-16 17:57:24 +0000

    'Solution to Problem-10' drafted.

---
+++ plotfunction.py
@@ -0,0 +1,9 @@
+# Etem Kaya 16-Mar-2019
+
+# Solution to Problem-10.
+# File name: "plotfunction.py".
+
+# Problem-10: Write a program that displays a plot of the functions x, x2 and
+# 2x in the range [0, 4].
+
+
