41d30e4dec1ca08afb77c8075dea209a4d01e7ab
Etem Kaya <etem.kaya@gmail.com>
2019-03-16 00:46:21 +0000

    Second update to 'Solution to Problem-9'.

--- second.py
+++ second.py
@@ -8,11 +8,18 @@
 
 
 # open the text file containing first paragraph of Moby-Dick book in read-only mode.
-f = open('C:\\Users\\etemk\\Desktop\\pands-problem-set\\moby-dick.txt', 'r')
+text_file = open('C:\\Users\\etemk\\Desktop\\pands-problem-set\\moby-dick.txt', 'r')
 
-# read the whole text file and display on the screen.
-print(f.read())
+# assign every lines of the text file to a new variable called 'lines'.
+line = text_file.readlines()
+
+# loop through every second line of the text file 'moby-dick.txt',
+# starting from line zero then increment by 2 so every second line.
+for i in range(0, len(line), 2):
+    
+    # print every second line of the text file starting from index zero.
+    print (line[i])
 
 # close the file.
-f.close()
-print("The file is now closed")
+text_file.close()
+print("\nThe file is now closed. Goodbye.")
