c7768c64144691da7464b9efa8243b20f7d64a65
Etem Kaya <etem.kaya@gmail.com>
2019-03-16 00:13:05 +0000

    First update to 'Solution to Problem-9'.

--- second.py
+++ second.py
@@ -6,3 +6,13 @@
 # Problem-9: Write a program that reads in a text file and outputs every second line. 
 # The program should take the filename from an argument on the command line.
 
+
+# open the text file containing first paragraph of Moby-Dick book in read-only mode.
+f = open('C:\\Users\\etemk\\Desktop\\pands-problem-set\\moby-dick.txt', 'r')
+
+# read the whole text file and display on the screen.
+print(f.read())
+
+# close the file.
+f.close()
+print("The file is now closed")
